694dd98dd8809ca6a9f4f48930d7cd2f98cdaab6
moquan <moquan@users.noreply.github.com>
2019-10-17 13:34:09 +0100

    first version move to modules_torch.py

--- merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_pytorch.py
+++ merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_pytorch.py
@@ -8,6 +8,7 @@ numpy.random.seed(545)
 from modules import make_logger, read_file_list, prepare_file_path, prepare_file_path_list, make_held_out_file_number, copy_to_scratch
 from modules import keep_by_speaker, remove_by_speaker, keep_by_file_number, remove_by_file_number, keep_by_min_max_file_number, check_and_change_to_list
 from modules_2 import compute_feat_dim, log_class_attri, resil_nn_file_list, norm_nn_file_list, get_utters_from_binary_dict, count_male_female_class_errors
+from modules_torch import torch_initialisation
 
 from io_funcs.binary_io import BinaryIOCollection
 io_fun = BinaryIOCollection()
@@ -160,351 +161,17 @@ class dv_y_cmp_configuration(dv_y_configuration):
             {'type':'LinDV', 'size':self.dv_dim, 'num_channels':1, 'dropout_p':0}
         ]
 
-        # self.dv_y_model_class = dv_y_cmp_model
-        # self.make_feed_dict_method = make_feed_dict_y_cmp
+        from modules_torch import DV_Y_CMP_model
+        self.dv_y_model_class = DV_Y_CMP_model
+        self.make_feed_dict_method = make_feed_dict_y_cmp
 
         self.auto_complete(cfg)
-
-
-import torch
-torch.manual_seed(545)
-
-########################
-# PyTorch-based Layers #
-########################
-
-class Build_S_B_TD_Input_Layer(object):
-    ''' This layer has only parameters, no torch.nn.module '''
-    def __init__(self, dv_y_cfg):
-        self.input_dim = dv_y_cfg.batch_seq_len * dv_y_cfg.feat_dim
-        self.params = {}
-        self.params["output_dim_seq"]      = ['S', 'B', 'D']
-        self.params["output_dim_values"]   = {'S':dv_y_cfg.batch_num_spk, 'B':dv_y_cfg.spk_num_seq, 'D':self.input_dim}
-        v = self.params["output_dim_values"]
-        self.params["output_shape_values"] = [v['S'], v['B'], v['D']]
-
-def Build_S_B_D_Output_Layer(object):
-    ''' This layer has only parameters, no torch.nn.module '''
-    def __init__(self, model, dv_y_cfg):
-        self.input_dim  = dv_y_cfg.dv_dim
-        self.output_dim = dv_y_cfg.num_speaker_dict['train']
-        self.nn_layer = torch.nn.Linear(self.input_dim, self.output_dim)
-
-
-    def __call__(self, x):
-        ''' Simulate PyTorch forward() method '''
-        return self.nn_layer(x)
-
-class Tensor_Reshape(torch.nn.Module):
-    def __init__(self, current_layer_params):
-        super().__init__()
-        self.params = current_layer_params
-
-    def update_layer_params(self):
-        input_dim_seq = self.params['input_dim_seq']
-        input_dim_values = self.params['input_dim_values']
-        expect_input_dim_seq = self.params['expect_input_dim_seq']
-
-        # First, check if change is needed at all; pass on if not
-        if input_dim_seq == expect_input_dim_seq:
-            self.params['expect_input_dim_values'] = input_dim_values
-            return self.params
-        else:
-            # Make anything into ['S', 'B', 'T', 'D']
-            if input_dim_seq == ['S', 'B', 'T', 'D']:
-                # Do nothing, pass on
-                temp_input_dim_values = input_dim_values
-
-        # Then, make from ['S', 'B', 'T', 'D']
-        if expect_input_dim_seq == ['S', 'B', 'D']:
-            # So basically, stack and remove T; last dimension D -> T * D
-            expect_input_shape_values = [temp_input_dim_values['S'], temp_input_dim_values['B'], temp_input_dim_values['T']*temp_input_dim_values['D']]
-            self.params['expect_input_dim_values'] = {'S':temp_input_dim_values['S'], 'B':temp_input_dim_values['B'], 'T':0, 'D':temp_input_dim_values['T']*temp_input_dim_values['D'] }
-        return self.params
-
-    def forward(self, x):
-        input_dim_seq = self.params['input_dim_seq']
-        input_dim_values = self.params['input_dim_values']
-        expect_input_dim_seq = self.params['expect_input_dim_seq']
-        expect_input_dim_values = self.params['expect_input_dim_values']
-
-        # First, check if change is needed at all; pass on if not
-        if input_dim_seq == expect_input_dim_seq:
-            return x
-        else:
-            # Make anything into ['S', 'B', 'T', 'D']
-            if input_dim_seq == ['S', 'B', 'T', 'D']:
-                # Do nothing, pass on
-                temp_input = x
-
-        # Then, make from ['S', 'B', 'T', 'D']
-        if expect_input_dim_seq == ['S', 'B', 'D']:
-            # So basically, stack and remove T; last dimension D -> T * D
-            expect_input_shape_values = [expect_input_dim_values['S'], expect_input_dim_values['B'], expect_input_dim_values['D']]
-            expect_input = temp_input.view(expect_input_shape_values)
-        return expect_input
-
-class ReLUDVMaxLayer(torch.nn.Module):
-    def __init__(self, input_dim, output_dim, num_channels):
-        super().__init__()
-        self.input_dim    = input_dim
-        self.output_dim   = output_dim
-        self.num_channels = num_channels
-
-        self.fc_list = torch.nn.ModuleList([torch.nn.Linear(input_dim, output_dim) for i in range(self.num_channels)])
-        self.relu_fn = torch.nn.ReLU()
-
-    def forward(self, x):
-        h_list = []
-        for i in range(self.num_channels):
-            # Linear
-            h_i = self.fc_list[i](x)
-            # ReLU
-            h_i = self.relu_fn(h_i)
-            h_list.append(h_i)
-
-        h_stack = torch.stack(h_list, dim=0)
-        # MaxOut
-        h_max, _indices = torch.max(h_stack, dim=0, keepdim=False)
-        return h_max
-
-class Build_NN_Layer(torch.nn.Module):
-    def __init__(self, layer_config, prev_layer):
-        super().__init__()
-        self.params = {}
-        self.params["layer_config"] = layer_config
-        self.params["type"] = layer_config['type']
-        self.params["size"] = layer_config['size']
-
-        self.params["input_dim_seq"]    = prev_layer.params["output_dim_seq"]
-        self.params["input_dim_values"] = prev_layer.params["output_dim_values"]
-
-        # To be set per layer type; mostly for definition of h
-        self.params["expect_input_dim_seq"]    = []
-        self.params["expect_input_dim_values"] = {}
-        self.params["output_dim_seq"]          = []
-        self.params["output_dim_values"]       = {}
-
-        construct_layer = getattr(self, self.params["layer_config"]["type"])
-        construct_layer()
-
-        ''' Dropout '''
-        try: 
-            self.params["dropout_p"] = self.params["layer_config"]['dropout_p']
-        except KeyError: 
-            self.params["dropout_p"] = 0.
-            return dropout_input
-        if self.params["dropout_p"] > 0:
-            self.dropout_fn = torch.nn.Dropout(p=self.params["dropout_p"])
-        else:
-            self.dropout_fn = lambda a: a # Do nothing, just return same tensor
-
-    def forward(self, x):
-        x = self.reshape_fn(x)
-        x = self.layer_fn(x)
-        x = self.dropout_fn(x)
-        return x
-
-    def ReLUDVMax(self):
-        self.params["expect_input_dim_seq"] = ['S','B','D']
-        self.reshape_fn = Tensor_Reshape(self.params)
-        self.params = self.reshape_fn.update_layer_params()
-
-        self.params["output_dim_seq"]       = ['S', 'B', 'D']
-        v = self.params["expect_input_dim_values"]
-        self.params["output_dim_values"]    = {'S': v['S'], 'B': v['B'], 'D': self.params["size"]}
-
-        input_dim  = self.params['expect_input_dim_values']['D']
-        output_dim = self.params['output_dim_values']['D']
-        num_channels = self.params["layer_config"]["num_channels"]
-        self.layer_fn = ReLUDVMaxLayer(input_dim, output_dim, num_channels)
-
-    def LinDV(self):
-        self.params["expect_input_dim_seq"] = ['S','B','D']
-        self.reshape_fn = Tensor_Reshape(self.params)
-        self.params = self.reshape_fn.update_layer_params()
-
-        self.params["output_dim_seq"]       = ['S', 'B', 'D']
-        v = self.params["expect_input_dim_values"]
-        self.params["output_dim_values"]    = {'S': v['S'], 'B': v['B'], 'D': self.params["size"]}
-
-        input_dim  = self.params['expect_input_dim_values']['D']
-        output_dim = self.params['output_dim_values']['D']
-        self.layer_fn = torch.nn.Linear(input_dim, output_dim)
-
-
-########################
-# PyTorch-based Models #
-########################
-
-class DV_Y_CMP_NN_model(torch.nn.Module):
-    ''' S_B_D input, SB_D logit output '''
-    def __init__(self, dv_y_cfg):
-        super().__init__()
-        
-        self.num_nn_layers = dv_y_cfg.num_nn_layers
-        self.train_by_window = dv_y_cfg.train_by_window
-
-        self.input_layer = Build_S_B_TD_Input_Layer(dv_y_cfg)
-        self.input_dim   = self.input_layer.input_dim
-        prev_layer = self.input_layer
-
-        # Hidden layers
-        # The last is bottleneck, output_dim is lambda_dim
-        self.layer_list = torch.nn.ModuleList()
-        for i in range(self.num_nn_layers):
-            layer_config = dv_y_cfg.nn_layer_config_list[i]
-
-            layer_temp = Build_NN_Layer(layer_config, prev_layer)
-            prev_layer = layer_temp
-            self.layer_list.append(layer_temp)
-
-        # Expansion layer, from lambda to logit
-        self.dv_dim  = dv_y_cfg.dv_dim
-        self.output_dim = dv_y_cfg.num_speaker_dict['train']
-        self.expansion_layer = torch.nn.Linear(self.dv_dim, self.output_dim)
-
-    def gen_lambda_SBD(self, x):
-        for i in range(self.num_nn_layers):
-            layer_temp = self.layer_list[i]
-            x = layer_temp(x)
-        return x
-
-    def gen_logit_SBD(self, x):
-        lambda_SBD = self.gen_lambda_SBD(x)
-        logit_SBD  = self.expansion_layer(lambda_SBD)
-        return logit_SBD
-
-    def forward(self, x):
-        logit_SBD = self.gen_logit_SBD(x)
-        if self.train_by_window:
-            # Flatten to 2D for cross-entropy
-            logit_SB_D = logit_SBD.view(-1, self.output_dim)
-            return logit_SB_D
-        else:
-            # Average over B
-            logit_S_D = torch.mean(logit_SBD, dim=1, keepdim=False)
-
-class General_Model(object):
-    ''' Model Wrapper, handy between Python and PyTorch '''
-    def __init__(self):
-        self.nn_model = None
-
-    def __call__(self, x):
-        ''' Simulate PyTorch forward() method '''
-        ''' Note that x could be feed_dict '''
-        return self.nn_model(x)
-
-    def eval(self):
-        ''' Simulate PyTorch eval() method '''
-        self.nn_model.eval()
-
-    def train(self):
-        ''' Simulate PyTorch train() method '''
-        self.nn_model.train()
-
-    def to_device(self, device_id):
-        self.device_id = device_id
-        self.nn_model.to(device_id)
-
-    def print_model_parameters(self, logger):
-        logger.info('Print Parameter Sizes')
-        for name, param in self.nn_model.named_parameters():
-            print(str(name)+'  '+str(param.size())+'  '+str(param.type()))
-
-    def update_parameters(self, feed_dict):
-        self.loss = self.gen_loss(feed_dict)
-        # perform a backward pass, and update the weights.
-        self.loss.backward()
-        self.optimiser.step()
-
-    def update_learning_rate(self, learning_rate):
-        self.learning_rate = learning_rate
-        # Re-build an optimiser, use new learning rate, and reset gradients
-        self.build_optimiser()
-
-
-    def gen_loss_value(self, feed_dict):
-        self.loss = self.gen_loss(feed_dict)
-        return self.loss.item()
-
-    def save_nn_model(self, nnets_file_name):
-        save_dict = {'model_state_dict': self.nn_model.state_dict()}
-        torch.save(save_dict, nnets_file_name)
-
-    def load_nn_model(self, nnets_file_name):
-        checkpoint = torch.load(nnets_file_name)
-        self.nn_model.load_state_dict(checkpoint['model_state_dict'])
-
-    def save_nn_model_optim(self, nnets_file_name):
-        save_dict = {'model_state_dict': self.nn_model.state_dict(), 'optimiser_state_dict': self.optimiser.state_dict()}
-        torch.save(save_dict, nnets_file_name)
-
-    def load_nn_model_optim(self, nnets_file_name):
-        checkpoint = torch.load(nnets_file_name)
-        self.nn_model.load_state_dict(checkpoint['model_state_dict'])
-        self.optimiser.load_state_dict(checkpoint['optimiser_state_dict'])
-
-class DV_Y_CMP_model(General_Model):
-    ''' S_B_D input, SB_D logit output, classification, cross-entropy '''
-    def __init__(self, dv_y_cfg):
-        super().__init__()
-        self.nn_model = DV_Y_CMP_NN_model(dv_y_cfg)
-        self.learning_rate = dv_y_cfg.learning_rate
-
-    def build_optimiser(self):
-        self.criterion = torch.nn.CrossEntropyLoss(reduction='mean')
-        self.optimiser = torch.optim.Adam(self.nn_model.parameters(), lr=self.learning_rate)
-        # Zero gradients
-        self.optimiser.zero_grad()
-
-    def gen_loss(self, feed_dict):
-        ''' Returns Tensor, not value! For value, use gen_loss_value '''
-        x, y = self.numpy_to_tensor(feed_dict)
-        y_pred = self.nn_model(x)
-        # Compute and print loss
-        self.loss = self.criterion(y_pred, y)
-        return self.loss
-
-    def cal_accuracy(self, feed_dict):
-        x, y = self.numpy_to_tensor(feed_dict)
-        outputs = self.nn_model(x)
-        _, predicted = torch.max(outputs.data, 1)
-        total = y.size(0)
-        correct = (predicted == y).sum().item()
-        accuracy = correct/total
-        return correct, total, accuracy
-
-    def numpy_to_tensor(self, feed_dict):
-        x_val = feed_dict['x']
-        y_val = feed_dict['y']
-        x = torch.tensor(x_val, dtype=torch.float)
-        y = torch.tensor(y_val, dtype=torch.long)
-        x = x.to(self.device_id)
-        y = y.to(self.device_id)
-        return (x, y)
-
-
-def torch_initialisation(dv_y_cfg):
-    logger = make_logger("torch initialisation")
-    if torch.cuda.is_available():
-    # if False:
-        logger.info('Using GPU cuda:%i' % dv_y_cfg.gpu_id)
-        device_id = torch.device("cuda:%i" % dv_y_cfg.gpu_id)
-    else:
-        logger.info('Using CPU; No GPU')
-        device_id = torch.device("cpu")
-
-    # Construct our model by instantiating the class defined above
-    model = DV_Y_CMP_model(dv_y_cfg)
-    model.to_device(device_id)
-    return model
 
 #############
 # Processes #
 #############
 
-def make_feed_dict_y_cmp(dv_y_cfg, file_list_dict, file_dir_dict, dv_y_model, batch_speaker_list, utter_tvt, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
+def make_feed_dict_y_cmp(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
     feat_name = dv_y_cfg.y_feat_name # Hard-coded here for now
     # Make i/o shape arrays
     # This is numpy shape, not Tensor shape!
@@ -566,12 +233,9 @@ def make_feed_dict_y_cmp(dv_y_cfg, file_list_dict, file_dir_dict, dv_y_model, ba
         return_list.append(file_name_list)
     return return_list
 
-def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
+def train_dv_y_model(cfg, dv_y_cfg):
 
-    # First attempt: 3 layers of ReLUMax
-    # Also, feed data use feed_dict style
-
-    if dv_y_cfg is None: dv_y_cfg = dv_y_cmp_configuration(cfg)
+    # Feed data use feed_dict style
 
     logger = make_logger("dv_y_config")
     log_class_attri(dv_y_cfg, logger, except_list=dv_y_cfg.log_except_list)
@@ -581,6 +245,7 @@ def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
     speaker_id_list = dv_y_cfg.speaker_id_list_dict['train'] # For DV training and evaluation, use train speakers only
     file_id_list    = read_file_list(cfg.file_id_list_file)
     file_list_dict  = make_dv_file_list(file_id_list, speaker_id_list, dv_y_cfg.data_split_file_number) # In the form of: file_list[(speaker_id, 'train')]
+    make_feed_dict_method = dv_y_cfg.make_feed_dict_method
 
     dv_y_model = torch_initialisation(dv_y_cfg)
     dv_y_model.build_optimiser()
@@ -604,7 +269,7 @@ def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
             # Draw random speakers
             batch_speaker_list = numpy.random.choice(speaker_id_list, dv_y_cfg.batch_num_spk)
             # Make feed_dict for training
-            feed_dict, batch_size = make_feed_dict_y_cmp(dv_y_cfg, file_list_dict, cfg.nn_feat_scratch_dirs, dv_y_model, batch_speaker_list,  utter_tvt='train')
+            feed_dict, batch_size = make_feed_dict_method(dv_y_cfg, file_list_dict, cfg.nn_feat_scratch_dirs, batch_speaker_list,  utter_tvt='train')
             dv_y_model.nn_model.train()
             dv_y_model.update_parameters(feed_dict=feed_dict)
         epoch_train_time = time.time()
@@ -619,7 +284,7 @@ def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
                 # Draw random speakers
                 batch_speaker_list = numpy.random.choice(speaker_id_list, dv_y_cfg.batch_num_spk)
                 # Make feed_dict for evaluation
-                feed_dict, batch_size = make_feed_dict_y_cmp(dv_y_cfg, file_list_dict, cfg.nn_feat_scratch_dirs, dv_y_model, batch_speaker_list, utter_tvt=utter_tvt_name)
+                feed_dict, batch_size = make_feed_dict_method(dv_y_cfg, file_list_dict, cfg.nn_feat_scratch_dirs, batch_speaker_list, utter_tvt=utter_tvt_name)
                 dv_y_model.eval()
                 batch_mean_loss = dv_y_model.gen_loss_value(feed_dict=feed_dict)
                 total_batch_size += batch_size
@@ -674,29 +339,7 @@ def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
 
 
 
-def data_format_test(dv_y_cfg, dv_y_model):
-    logger = make_logger("data_format_test")
-    S = dv_y_cfg.batch_num_spk
-    B = dv_y_cfg.spk_num_seq
-    T = dv_y_cfg.batch_seq_len
-    D = dv_y_cfg.feat_dim
-    D_in  = T * D
-    D_out = dv_y_cfg.num_speaker_dict['train']
-    
-    # Create random Tensors to hold inputs and outputs
-    x_val = numpy.random.rand(S,B,D_in)
-    y_val = numpy.ones(S*B)
-    x = torch.tensor(x_val, dtype=torch.float)
-    y = torch.tensor(y_val, dtype=torch.long)
-
-    feed_dict = {'x':x_val, 'y':y_val}
-    
-    for t in range(1,501):
-        dv_y_model.nn_model.train()
-        dv_y_model.update_parameters(feed_dict)
-        if t % 100 == 0:
-            dv_y_model.nn_model.eval()
-            loss = dv_y_model.gen_loss_value(feed_dict)
-            logger.info('%i, %f' % (t, loss))
-        
+def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
+    if dv_y_cfg is None: dv_y_cfg = dv_y_cmp_configuration(cfg)
+    train_dv_y_model(cfg, dv_y_cfg)
 
