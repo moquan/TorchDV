8a4dfc51d95a0d1746b7e0de008751991b879f1d
moquan <moquan@users.noreply.github.com>
2019-10-23 17:54:10 +0100

    Update modules_2.py
    add a module that prints and returns f0_mean_var; for sinenet

--- merlin_cued_mw545_pytorch/modules_2.py
+++ merlin_cued_mw545_pytorch/modules_2.py
@@ -217,3 +217,17 @@ def count_male_female_class_errors(total_wrong_class, male_speaker_list):
         str_temp = list_temp[0] + list_temp[1]
         wrong_list[str_temp] += count_temp
     return wrong_list
+
+def print_f0_mean_var(norm_file=None):
+    if norm_file == None:
+        norm_file = 'data/nn_cmp_resil_norm_86_info.dat'
+    from frontend.mean_variance_norm import MeanVarianceNorm
+    mean_var_normaliser = MeanVarianceNorm(feature_dimension=86)
+    mean_var_normaliser.load_mean_var_values(norm_file)
+    mean_vector = mean_var_normaliser.mean_vector
+    std_vector  = mean_var_normaliser.std_vector
+    print(mean_vector)
+    print(std_vector)
+    print(mean_vector[60])
+    print(std_vector[60])
+    return mean_vector[60], std_vector[60]
