d3af6f891942263df30938ab454e855e3d2c0cb9
moquan <moquan@users.noreply.github.com>
2019-11-07 16:53:13 +0000

    Update exp_dv_cmp_baseline.py

--- merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_baseline.py
+++ merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_baseline.py
@@ -14,7 +14,7 @@ from modules_torch import torch_initialisation
 from io_funcs.binary_io import BinaryIOCollection
 io_fun = BinaryIOCollection()
 
-from exp_mw545.exp_dv_cmp_pytorch import list_random_loader, dv_y_configuration, make_dv_y_exp_dir_name, make_dv_file_list, train_dv_y_model, class_test_dv_y_model
+from exp_mw545.exp_dv_cmp_pytorch import list_random_loader, dv_y_configuration, make_dv_y_exp_dir_name, make_dv_file_list, train_dv_y_model, class_test_dv_y_model, distance_test_dv_y_cmp_model
 
 
 def make_feed_dict_y_cmp_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
@@ -163,7 +163,7 @@ class dv_y_cmp_configuration(dv_y_configuration):
         self.batch_seq_total_len = 400 # Number of frames at 200Hz; 400 for 2s
         self.batch_seq_len   = 40 # T
         self.batch_seq_shift = 5
-        self.dv_dim = 64
+        self.dv_dim = 256
         self.nn_layer_config_list = [
             # Must contain: type, size; num_channels, dropout_p are optional, default 0, 1
             # {'type':'SineAttenCNN', 'size':512, 'num_channels':1, 'dropout_p':1, 'CNN_filter_size':5, 'Sine_filter_size':200,'lf0_mean':5.04976, 'lf0_var':0.361811},
@@ -171,8 +171,8 @@ class dv_y_cmp_configuration(dv_y_configuration):
             {'type':'ReLUDVMax', 'size':256, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0, 'batch_norm':False},
             {'type':'ReLUDVMax', 'size':256, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0, 'batch_norm':False},
             # {'type':'ReLUDVMax', 'size':256, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0.5, 'batch_norm':False},
-            # {'type':'ReLUDVMax', 'size':self.dv_dim, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0.5, 'batch_norm':False}
-            {'type':'LinDV', 'size':self.dv_dim, 'num_channels':1, 'dropout_p':0.5}
+            {'type':'ReLUDVMax', 'size':self.dv_dim, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0.5, 'batch_norm':False}
+            # {'type':'LinDV', 'size':self.dv_dim, 'num_channels':1, 'dropout_p':0.5}
         ]
 
         from modules_torch import DV_Y_CMP_model
@@ -187,7 +187,6 @@ def train_dv_y_cmp_model(cfg, dv_y_cfg=None):
 
 def test_dv_y_cmp_model(cfg, dv_y_cfg=None):
     if dv_y_cfg is None: dv_y_cfg = dv_y_cmp_configuration(cfg)
-    # for s in [545,54,5]:
-        # numpy.random.seed(s)
-    class_test_dv_y_model(cfg, dv_y_cfg)
+    # class_test_dv_y_model(cfg, dv_y_cfg)
+    distance_test_dv_y_cmp_model(cfg, dv_y_cfg)
 
