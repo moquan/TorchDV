bd47aabf5115d5a9e4f2f45d7ce0c33fd7f27f29
moquan <moquan@users.noreply.github.com>
2020-02-25 13:45:52 +0000

    Update exp_dv_cmp_baseline.py
    remove dv_y_cfg.spk_num_utter; rename dv as one_hot

--- merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_baseline.py
+++ merlin_cued_mw545_pytorch/exp_mw545/exp_dv_cmp_baseline.py
@@ -18,12 +18,12 @@ io_fun = BinaryIOCollection()
 
 from exp_mw545.exp_dv_cmp_pytorch import list_random_loader, dv_y_configuration, make_dv_y_exp_dir_name, make_dv_file_list, train_dv_y_model, class_test_dv_y_model, distance_test_dv_y_model
 
-def make_feed_dict_y_cmp_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
+def make_feed_dict_y_cmp_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_one_hot=False, return_y=False, return_frame_index=False, return_file_name=False):
     feat_name = dv_y_cfg.y_feat_name # Hard-coded here for now
     # Make i/o shape arrays
     # This is numpy shape, not Tensor shape!
-    y  = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len, dv_y_cfg.feat_dim))
-    dv = numpy.zeros((dv_y_cfg.batch_num_spk))
+    y = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len, dv_y_cfg.feat_dim))
+    one_hot = numpy.zeros((dv_y_cfg.batch_num_spk))
     
     # Do not use silence frames at the beginning or the end
     total_sil_one_side_cmp = dv_y_cfg.frames_silence_to_keep + dv_y_cfg.sil_pad
@@ -36,47 +36,48 @@ def make_feed_dict_y_cmp_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_sp
 
         # Make classification targets, index sequence
         true_speaker_index = dv_y_cfg.speaker_id_list_dict['train'].index(speaker_id)
-        dv[speaker_idx] = true_speaker_index
+        one_hot[speaker_idx] = true_speaker_index
 
-        # Draw multiple utterances per speaker: dv_y_cfg.spk_num_utter
-        # Draw multiple windows per utterance:  dv_y_cfg.utter_num_seq
+        # Draw 1 utterance per speaker
+        # Draw multiple windows per utterance: dv_y_cfg.spk_num_seq
         # Stack them along B
-        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(dv_y_cfg.spk_num_utter, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=[feat_name], feat_dim_list=[dv_y_cfg.feat_dim], min_file_len=min_file_len, random_seed=None)
+        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(1, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=[feat_name], feat_dim_list=[dv_y_cfg.feat_dim], min_file_len=min_file_len, random_seed=None)
         file_name_list.append(speaker_file_name_list)
 
         speaker_start_frame_index_list = []
-        for utter_idx in range(dv_y_cfg.spk_num_utter):
-            file_name = speaker_file_name_list[utter_idx]
-            cmp_file = speaker_utter_list[feat_name][utter_idx][:,dv_y_cfg.feat_index]
-            cmp_file_len = speaker_utter_len_list[utter_idx]
 
-            if all_utt_start_frame_index is None:
-                # Use random starting frame index
-                extra_file_len = cmp_file_len - min_file_len
-                start_frame_index = numpy.random.randint(low=total_sil_one_side_cmp, high=total_sil_one_side_cmp+extra_file_len+1)
-            else:
-                start_frame_index = total_sil_one_side_cmp + all_utt_start_frame_index
-            speaker_start_frame_index_list.append(start_frame_index)
-            for seq_idx in range(dv_y_cfg.utter_num_seq):
-                y[speaker_idx, utter_idx*dv_y_cfg.utter_num_seq+seq_idx, :, :] = cmp_file[start_frame_index:start_frame_index+dv_y_cfg.batch_seq_len, :]
-                start_frame_index = start_frame_index + dv_y_cfg.batch_seq_shift
+        file_name = speaker_file_name_list[0]
+        cmp_file = speaker_utter_list[feat_name][0][:,dv_y_cfg.feat_index]
+        cmp_file_len = speaker_utter_len_list[0]
+
+        if all_utt_start_frame_index is None:
+            # Use random starting frame index
+            extra_file_len = cmp_file_len - min_file_len
+            start_frame_index = numpy.random.randint(low=total_sil_one_side_cmp, high=total_sil_one_side_cmp+extra_file_len+1)
+        else:
+            start_frame_index = total_sil_one_side_cmp + all_utt_start_frame_index
+        speaker_start_frame_index_list.append(start_frame_index)
+        for seq_idx in range(dv_y_cfg.spk_num_seq):
+            y[speaker_idx, seq_idx, :, :] = cmp_file[start_frame_index:start_frame_index+dv_y_cfg.batch_seq_len, :]
+            start_frame_index = start_frame_index + dv_y_cfg.batch_seq_shift
+                
         start_frame_index_list.append(speaker_start_frame_index_list)
 
     # S,B,T,D --> S,B,T*D
     x_val = numpy.reshape(y, (dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len*dv_y_cfg.feat_dim))
     if dv_y_cfg.train_by_window:
         # S --> S*B
-        y_val = numpy.repeat(dv, dv_y_cfg.spk_num_seq)
+        y_val = numpy.repeat(one_hot, dv_y_cfg.spk_num_seq)
         batch_size = dv_y_cfg.batch_num_spk * dv_y_cfg.spk_num_seq
     else:
-        y_val = dv
+        y_val = one_hot
         batch_size = dv_y_cfg.batch_num_spk
 
     feed_dict = {'x':x_val, 'y':y_val}
     return_list = [feed_dict, batch_size]
     
-    if return_dv:
-        return_list.append(dv)
+    if return_one_hot:
+        return_list.append(one_hot)
     if return_y:
         return_list.append(y)
     if return_frame_index:
@@ -92,12 +93,12 @@ def make_feed_dict_y_cmp_test(dv_y_cfg, file_dir_dict, speaker_id, file_name, st
     # This is numpy shape, not Tensor shape!
     # No speaker index here! Will add it to Tensor later
     y  = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len, dv_y_cfg.feat_dim))
-    dv = numpy.zeros((dv_y_cfg.batch_num_spk))
+    one_hot = numpy.zeros((dv_y_cfg.batch_num_spk))
 
     # Make classification targets, index sequence
     try: true_speaker_index = dv_y_cfg.speaker_id_list_dict['train'].index(speaker_id)
-    except ValueError: true_speaker_index = 0 # At generation time, since dv is not used, a non-train speaker is given an arbituary speaker index
-    dv[0] = true_speaker_index
+    except ValueError: true_speaker_index = 0 # At generation time, since one_hot is not used, a non-train speaker is given an arbituary speaker index
+    one_hot[0] = true_speaker_index
 
     if BTD_feat_remain is not None:
         cmp_feat_current = BTD_feat_remain
@@ -137,15 +138,15 @@ def make_feed_dict_y_cmp_test(dv_y_cfg, file_dir_dict, speaker_id, file_name, st
     x_val = numpy.reshape(y, (dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len*dv_y_cfg.feat_dim))
     if dv_y_cfg.train_by_window:
         # S --> S*B
-        y_val = numpy.repeat(dv, dv_y_cfg.spk_num_seq)
+        y_val = numpy.repeat(one_hot, dv_y_cfg.spk_num_seq)
     else:
-        y_val = dv
+        y_val = one_hot
 
     feed_dict = {'x':x_val, 'y':y_val}
     return_list = [feed_dict, gen_finish, batch_size, BTD_feat_remain]
     return return_list
 
-def make_feed_dict_y_cmp_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
+def make_feed_dict_y_cmp_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_y=False, return_frame_index=False, return_file_name=False):
     feat_name = dv_y_cfg.y_feat_name # Hard-coded here for now
     # Make i/o shape arrays
     # This is numpy shape, not Tensor shape!
@@ -165,34 +166,34 @@ def make_feed_dict_y_cmp_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch
     for speaker_idx in range(dv_y_cfg.batch_num_spk):
         speaker_id = batch_speaker_list[speaker_idx]
 
-        # Draw multiple utterances per speaker: dv_y_cfg.spk_num_utter
-        # Draw multiple windows per utterance:  dv_y_cfg.utter_num_seq
+        # Draw 1 utterance per speaker
+        # Draw multiple windows per utterance: dv_y_cfg.spk_num_seq
         # Stack them along B
-        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(dv_y_cfg.spk_num_utter, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=[feat_name], feat_dim_list=[dv_y_cfg.feat_dim], min_file_len=min_file_len, random_seed=None)
+        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(1, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=[feat_name], feat_dim_list=[dv_y_cfg.feat_dim], min_file_len=min_file_len, random_seed=None)
         file_name_list.append(speaker_file_name_list)
 
         speaker_start_frame_index_list = []
-        for utter_idx in range(dv_y_cfg.spk_num_utter):
-            file_name = speaker_file_name_list[utter_idx]
-            cmp_file = speaker_utter_list[feat_name][utter_idx][:,dv_y_cfg.feat_index]
-            cmp_file_len = speaker_utter_len_list[utter_idx]
 
-            if all_utt_start_frame_index is None:
-                # Use random starting frame index
-                extra_file_len = cmp_file_len - min_file_len
-                start_frame_index = numpy.random.randint(low=total_sil_one_side_cmp, high=total_sil_one_side_cmp+extra_file_len+1)
-            else:
-                start_frame_index = total_sil_one_side_cmp + all_utt_start_frame_index
-            speaker_start_frame_index_list.append(start_frame_index)
+        file_name = speaker_file_name_list[0]
+        cmp_file = speaker_utter_list[feat_name][0][:,dv_y_cfg.feat_index]
+        cmp_file_len = speaker_utter_len_list[0]
 
-            for plot_idx in range(dv_y_cfg.num_to_plot+1):
-                plot_start_frame_index = start_frame_index + plot_idx * dv_y_cfg.gap_len_to_plot
-                for seq_idx in range(dv_y_cfg.utter_num_seq):
-                    y_list[plot_idx][speaker_idx, utter_idx*dv_y_cfg.utter_num_seq+seq_idx, :, :] = cmp_file[plot_start_frame_index:plot_start_frame_index+dv_y_cfg.batch_seq_len, :]
-                    plot_start_frame_index = plot_start_frame_index + dv_y_cfg.batch_seq_shift
+        if all_utt_start_frame_index is None:
+            # Use random starting frame index
+            extra_file_len = cmp_file_len - min_file_len
+            start_frame_index = numpy.random.randint(low=total_sil_one_side_cmp, high=total_sil_one_side_cmp+extra_file_len+1)
+        else:
+            start_frame_index = total_sil_one_side_cmp + all_utt_start_frame_index
+        speaker_start_frame_index_list.append(start_frame_index)
+
+        for plot_idx in range(dv_y_cfg.num_to_plot+1):
+            plot_start_frame_index = start_frame_index + plot_idx * dv_y_cfg.gap_len_to_plot
+            for seq_idx in range(dv_y_cfg.spk_num_seq):
+                y_list[plot_idx][speaker_idx, seq_idx, :, :] = cmp_file[plot_start_frame_index:plot_start_frame_index+dv_y_cfg.batch_seq_len, :]
+                plot_start_frame_index = plot_start_frame_index + dv_y_cfg.batch_seq_shift
         start_frame_index_list.append(speaker_start_frame_index_list)
 
-    feed_dict_list = [{}] * (dv_y_cfg.num_to_plot+1)
+    feed_dict_list = [{} for i in range(dv_y_cfg.num_to_plot+1)]
     for plot_idx in range(dv_y_cfg.num_to_plot+1):
         # S,B,T,D --> S,B,T*D
         x_val = numpy.reshape(y_list[plot_idx], (dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.batch_seq_len*dv_y_cfg.feat_dim))
@@ -200,8 +201,6 @@ def make_feed_dict_y_cmp_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch
     batch_size  = dv_y_cfg.batch_num_spk * dv_y_cfg.spk_num_seq
     return_list = [feed_dict_list, batch_size]
     
-    if return_dv:
-        return_list.append(dv)
     if return_y:
         return_list.append(y)
     if return_frame_index:
@@ -233,9 +232,9 @@ class dv_y_cmp_configuration(dv_y_configuration):
             # Must contain: type, size; num_channels, dropout_p are optional, default 0, 1
             # {'type':'ReLUDVMax', 'size':256, 'num_channels':2, 'channel_combi':'maxout', 'dropout_p':0, 'batch_norm':False},
             # {'type':'LinDV', 'size':self.dv_dim, 'num_channels':1, 'dropout_p':0.5}
-            {'type':'ReLUDV', 'size':256, 'dropout_p':0, 'batch_norm':False},
             {'type':'ReLUDV', 'size':256, 'dropout_p':0, 'batch_norm':True},
-            {'type':'ReLUDV', 'size':self.dv_dim, 'dropout_p':0.2, 'batch_norm':False}
+            {'type':'LReLUDV', 'size':256, 'dropout_p':0, 'batch_norm':True},
+            {'type':'LReLUDV', 'size':self.dv_dim, 'dropout_p':0.2, 'batch_norm':False}
         ]
 
         self.gpu_id = 1
