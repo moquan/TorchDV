2871f2b46adbfe3935bbe0cd4a785081c13d4f31
moquan <moquan@users.noreply.github.com>
2020-02-25 21:52:59 +0000

    Update exp_dv_wav_subwin.py
    major update; use pytorch unfold method instead of numpy window method, speed-up

--- merlin_cued_mw545_pytorch/exp_mw545/exp_dv_wav_subwin.py
+++ merlin_cued_mw545_pytorch/exp_mw545/exp_dv_wav_subwin.py
@@ -5,6 +5,7 @@
 
 # For each window, network input is a vector of stacked waveforms
 # Then within each window, split into smaller windows (M)
+# Both slicings are performed in pytorch, not numpy
 
 import os, sys, pickle, time, shutil, logging, copy
 import math, numpy, scipy
@@ -19,21 +20,19 @@ io_fun = BinaryIOCollection()
 
 from exp_mw545.exp_dv_cmp_pytorch import list_random_loader, dv_y_configuration, make_dv_y_exp_dir_name, make_dv_file_list, train_dv_y_model, class_test_dv_y_model, distance_test_dv_y_model
 
-def make_feed_dict_y_wav_subwin_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
+def make_feed_dict_y_wav_subwin_train(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_one_hot=False, return_y=False, return_frame_index=False, return_file_name=False):
     logger = make_logger("make_dict")
 
     '''
     Draw Utterances; Load Data
     Draw starting frame; Slice; Fit into numpy holders
     '''
-    load_time  = 0.
-    slice_time = 0.
     feat_name_list = ['wav'] # Load wav
     feat_dim_list  = [1]
     # Make i/o shape arrays
     # This is numpy shape, not Tensor shape!
-    wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.seq_num_win, dv_y_cfg.seq_win_len))
-    dv  = numpy.zeros((dv_y_cfg.batch_num_spk))
+    wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.batch_seq_total_len))
+    one_hot = numpy.zeros((dv_y_cfg.batch_num_spk))
 
     wav_sr  = dv_y_cfg.cfg.wav_sr
     cmp_sr  = dv_y_cfg.cfg.frame_sr
@@ -51,48 +50,32 @@ def make_feed_dict_y_wav_subwin_train(dv_y_cfg, file_list_dict, file_dir_dict, b
         speaker_id = batch_speaker_list[speaker_idx]
         # Make classification targets, index sequence
         true_speaker_index = dv_y_cfg.speaker_id_list_dict['train'].index(speaker_id)
-        dv[speaker_idx] = true_speaker_index
+        one_hot[speaker_idx] = true_speaker_index
 
-        # Draw multiple utterances per speaker: dv_y_cfg.spk_num_utter
-        # Draw multiple windows per utterance:  dv_y_cfg.utter_num_seq
-        # Stack them along B
-        speaker_start_time = time.time()
-        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(dv_y_cfg.spk_num_utter, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=feat_name_list, feat_dim_list=feat_dim_list, min_file_len=min_file_len, random_seed=None)
+        # Draw 1 utterance per speaker
+        # Draw multiple windows per utterance; multiple sub-windows per window; in pytorch, not here
+
+        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(1, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=feat_name_list, feat_dim_list=feat_dim_list, min_file_len=min_file_len, random_seed=None)
         file_name_list.append(speaker_file_name_list)
-        speaker_load_time = time.time()
-        load_time += (speaker_load_time-speaker_start_time)
 
-        for utter_idx in range(dv_y_cfg.spk_num_utter):
-            file_name = speaker_file_name_list[utter_idx]
-            wav_file  = speaker_utter_list['wav'][utter_idx] # T * 1; 16kHz
-            wav_file  = numpy.squeeze(wav_file, axis=1)      # T*1 -> T
-            wav_file_len = speaker_utter_len_list[utter_idx]
+        file_name = speaker_file_name_list[0]
+        wav_file  = speaker_utter_list['wav'][0] # T * 1; 16kHz
+        wav_file  = numpy.squeeze(wav_file, axis=1)      # T*1 -> T
+        wav_file_len = speaker_utter_len_list[0]
 
-            # Find start frame index, random if None
-            if all_utt_start_frame_index is None:
-                extra_file_len = wav_file_len - min_file_len
-                utter_start_frame_index = numpy.random.randint(low=total_sil_one_side_wav, high=total_sil_one_side_wav+extra_file_len+1)
-            else:
-                utter_start_frame_index = total_sil_one_side_wav + all_utt_start_frame_index
-            start_frame_index_list[speaker_idx].append(utter_start_frame_index)
+        # Find start frame index, random if None
+        if all_utt_start_frame_index is None:
+            extra_file_len = wav_file_len - min_file_len
+            utter_start_frame_index = numpy.random.randint(low=total_sil_one_side_wav, high=total_sil_one_side_wav+extra_file_len+1)
+        else:
+            utter_start_frame_index = total_sil_one_side_wav + all_utt_start_frame_index
+        start_frame_index_list[speaker_idx].append(utter_start_frame_index)
+        wav[speaker_idx, :] = wav_file[utter_start_frame_index:utter_start_frame_index+dv_y_cfg.batch_seq_total_len]
 
-            # Slice data into seq and win
-            for seq_idx in range(dv_y_cfg.utter_num_seq):
-                spk_seq_index = utter_idx * dv_y_cfg.utter_num_seq + seq_idx
-                seq_start = utter_start_frame_index + seq_idx * dv_y_cfg.batch_seq_shift
-                for win_idx in range(dv_y_cfg.seq_num_win):
-                    win_start = seq_start + win_idx * dv_y_cfg.seq_win_shift
-                    win_end   = win_start + dv_y_cfg.seq_win_len - 1 # Inclusive index
-                    wav[speaker_idx, spk_seq_index, win_idx, :] = wav_file[win_start:win_end+1]
-        speaker_slice_time = time.time()
-        slice_time += (speaker_slice_time-speaker_load_time)
-    print('Load time is %s, Slice time is %s' %(str(load_time), str(slice_time)))
-
-    # S,B,M,D
     x_val = wav
     if dv_y_cfg.train_by_window:
         # S --> S*B
-        y_val = numpy.repeat(dv, dv_y_cfg.spk_num_seq)
+        y_val = numpy.repeat(one_hot, dv_y_cfg.spk_num_seq)
         batch_size = dv_y_cfg.batch_num_spk * dv_y_cfg.spk_num_seq
     else:
         y_val = one_hot
@@ -101,8 +84,8 @@ def make_feed_dict_y_wav_subwin_train(dv_y_cfg, file_list_dict, file_dir_dict, b
     feed_dict = {'x':x_val, 'y':y_val}
     return_list = [feed_dict, batch_size]
     
-    if return_dv:
-        return_list.append(dv)
+    if return_one_hot:
+        return_list.append(one_hot)
     if return_y:
         return_list.append(y)
     if return_frame_index:
@@ -123,17 +106,17 @@ def make_feed_dict_y_wav_subwin_test(dv_y_cfg, file_dir_dict, speaker_id, file_n
     assert dv_y_cfg.batch_num_spk == 1
     # Make i/o shape arrays
     # This is numpy shape, not Tensor shape!
-    wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.seq_num_win, dv_y_cfg.seq_win_len))
-    dv  = numpy.zeros((dv_y_cfg.batch_num_spk))
+    wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.batch_seq_total_len))
+    one_hot = numpy.zeros((dv_y_cfg.batch_num_spk))
 
     # Make classification targets, index sequence
     try: true_speaker_index = dv_y_cfg.speaker_id_list_dict['train'].index(speaker_id)
-    except ValueError: true_speaker_index = 0 # At generation time, since dv is not used, a non-train speaker is given an arbituary speaker index
-    dv[0] = true_speaker_index
+    except ValueError: true_speaker_index = 0 # At generation time, since one_hot is not used, a non-train speaker is given an arbituary speaker index
+    one_hot[0] = true_speaker_index
 
     if BTD_feat_remain is not None:
         wav_feat_current = BTD_feat_remain
-        B_total = wav_feat_current.shape[0]
+        T_total = wav_feat_current.shape[0]
     else:
         # Get new file, make BTD
         file_min_len, features = get_one_utter_by_name(file_name, file_dir_dict, feat_name_list=feat_name_list, feat_dim_list=feat_dim_list)
@@ -152,46 +135,40 @@ def make_feed_dict_y_wav_subwin_test(dv_y_cfg, file_dir_dict, speaker_id, file_n
 
         # Make numpy holders for no_sil data
         wav_features_no_sil = wav_file[total_sil_one_side_wav:total_sil_one_side_wav+len_no_sil_wav]
-        B_total = int((len_no_sil_wav - dv_y_cfg.batch_seq_len) / dv_y_cfg.batch_seq_shift) + 1
-        wav_feat_current = numpy.zeros((B_total, dv_y_cfg.seq_num_win, dv_y_cfg.seq_win_len))
-
-        # Slice data into seq and win
-        for seq_idx in range(B_total):
-            spk_seq_index = seq_idx
-            seq_start = 0 + seq_idx * dv_y_cfg.batch_seq_shift
-            for win_idx in range(dv_y_cfg.seq_num_win):
-                win_start = seq_start + win_idx * dv_y_cfg.seq_win_shift
-                win_end   = win_start + dv_y_cfg.seq_win_len - 1 # Inclusive index
-                wav_feat_current[spk_seq_index, win_idx, :] = wav_file[win_start:win_end+1]
+        wav_feat_current = wav_features_no_sil
+        T_total = len_no_sil_wav
+        
+    B_total = int((T_total - dv_y_cfg.batch_seq_len) / dv_y_cfg.batch_seq_shift) + 1
 
     if B_total > dv_y_cfg.spk_num_seq:
         B_actual = dv_y_cfg.spk_num_seq
+        T_actual = dv_y_cfg.batch_seq_total_len
         B_remain = B_total - B_actual
         gen_finish = False
-        wav_feat_remain = wav_feat_current[B_actual:]
+        wav_feat_remain = wav_feat_current[B_actual*dv_y_cfg.batch_seq_shift:]
         BTD_feat_remain = wav_feat_remain
     else:
         B_actual = B_total
+        T_actual = T_total
         B_remain = 0
         gen_finish = True
         BTD_feat_remain = None
 
-    wav[0,:B_actual] = wav_feat_current[:B_actual]
+    wav[0,:T_actual] = wav_feat_current[:T_actual]
     batch_size = B_actual
 
-    # B,T,D --> S(1),B,T*D
     x_val = wav
     if dv_y_cfg.train_by_window:
         # S --> S*B
-        y_val = numpy.repeat(dv, dv_y_cfg.spk_num_seq)
+        y_val = numpy.repeat(one_hot, dv_y_cfg.spk_num_seq)
     else:
-        y_val = dv
+        y_val = one_hot
 
     feed_dict = {'x':x_val, 'y':y_val}
     return_list = [feed_dict, gen_finish, batch_size, BTD_feat_remain]
     return return_list
 
-def make_feed_dict_y_wav_subwin_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_dv=False, return_y=False, return_frame_index=False, return_file_name=False):
+def make_feed_dict_y_wav_subwin_distance(dv_y_cfg, file_list_dict, file_dir_dict, batch_speaker_list, utter_tvt, all_utt_start_frame_index=None, return_y=False, return_frame_index=False, return_file_name=False):
     logger = make_logger("make_dict")
 
     '''
@@ -204,7 +181,7 @@ def make_feed_dict_y_wav_subwin_distance(dv_y_cfg, file_list_dict, file_dir_dict
     # This is numpy shape, not Tensor shape!
     wav_list = []
     for plot_idx in range(dv_y_cfg.num_to_plot + 1):
-        wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.spk_num_seq, dv_y_cfg.seq_num_win, dv_y_cfg.seq_win_len))
+        wav = numpy.zeros((dv_y_cfg.batch_num_spk, dv_y_cfg.batch_seq_total_len))
         wav_list.append(wav)
 
     wav_sr  = dv_y_cfg.cfg.wav_sr
@@ -224,39 +201,30 @@ def make_feed_dict_y_wav_subwin_distance(dv_y_cfg, file_list_dict, file_dir_dict
 
         speaker_id = batch_speaker_list[speaker_idx]
 
-        # Draw multiple utterances per speaker: dv_y_cfg.spk_num_utter
-        # Draw multiple windows per utterance:  dv_y_cfg.utter_num_seq
-        # Stack them along B
-        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(dv_y_cfg.spk_num_utter, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=feat_name_list, feat_dim_list=feat_dim_list, min_file_len=min_file_len, random_seed=None)
+        # Draw 1 utterance per speaker
+        # Draw multiple windows per utterance; multiple sub-windows per window; in pytorch, not here
+
+        speaker_file_name_list, speaker_utter_len_list, speaker_utter_list = get_utters_from_binary_dict(1, file_list_dict[(speaker_id, utter_tvt)], file_dir_dict, feat_name_list=feat_name_list, feat_dim_list=feat_dim_list, min_file_len=min_file_len, random_seed=None)
         file_name_list.append(speaker_file_name_list)
 
-        for utter_idx in range(dv_y_cfg.spk_num_utter):
-            file_name = speaker_file_name_list[utter_idx]
-            wav_file  = speaker_utter_list['wav'][utter_idx] # T * 1; 16kHz
-            wav_file  = numpy.squeeze(wav_file, axis=1)      # T*1 -> T
-            wav_file_len = speaker_utter_len_list[utter_idx]
+        file_name = speaker_file_name_list[0]
+        wav_file  = speaker_utter_list['wav'][0] # T * 1; 16kHz
+        wav_file  = numpy.squeeze(wav_file, axis=1)      # T*1 -> T
+        wav_file_len = speaker_utter_len_list[0]
 
-            # Find start frame index, random if None
-            if all_utt_start_frame_index is None:
-                extra_file_len = wav_file_len - min_file_len
-                utter_start_frame_index = numpy.random.randint(low=total_sil_one_side_wav, high=total_sil_one_side_wav+extra_file_len+1)
-            else:
-                utter_start_frame_index = total_sil_one_side_wav + all_utt_start_frame_index
-            start_frame_index_list[speaker_idx].append(utter_start_frame_index)
+        # Find start frame index, random if None
+        if all_utt_start_frame_index is None:
+            extra_file_len = wav_file_len - min_file_len
+            utter_start_frame_index = numpy.random.randint(low=total_sil_one_side_wav, high=total_sil_one_side_wav+extra_file_len+1)
+        else:
+            utter_start_frame_index = total_sil_one_side_wav + all_utt_start_frame_index
+        start_frame_index_list[speaker_idx].append(utter_start_frame_index)
 
-            for plot_idx in range(dv_y_cfg.num_to_plot+1):
-                plot_start_frame_index = utter_start_frame_index + plot_idx * dv_y_cfg.gap_len_to_plot
-                # Slice data into seq and win
-                for seq_idx in range(dv_y_cfg.utter_num_seq):
-                    spk_seq_index = utter_idx * dv_y_cfg.utter_num_seq + seq_idx
-                    seq_start = plot_start_frame_index + seq_idx * dv_y_cfg.batch_seq_shift
-                    for win_idx in range(dv_y_cfg.seq_num_win):
-                        win_start = seq_start + win_idx * dv_y_cfg.seq_win_shift
-                        win_end   = win_start + dv_y_cfg.seq_win_len - 1 # Inclusive index
-                        wav_list[plot_idx][speaker_idx, spk_seq_index, win_idx, :] = wav_file[win_start:win_end+1]
+        for plot_idx in range(dv_y_cfg.num_to_plot+1):
+            plot_start_frame_index = utter_start_frame_index + plot_idx * dv_y_cfg.gap_len_to_plot
+            wav_list[plot_idx][speaker_idx] = wav_file[plot_start_frame_index:plot_start_frame_index+dv_y_cfg.batch_seq_total_len]
 
-    # S,B,M,D
-    feed_dict_list = [{}] * (dv_y_cfg.num_to_plot+1)
+    feed_dict_list = [{} for i in range(dv_y_cfg.num_to_plot+1)]
     for plot_idx in range(dv_y_cfg.num_to_plot+1):
         x_val = wav_list[plot_idx]
         feed_dict_list[plot_idx] = {'x':x_val}
@@ -264,8 +232,6 @@ def make_feed_dict_y_wav_subwin_distance(dv_y_cfg, file_list_dict, file_dir_dict
 
     return_list = [feed_dict_list, batch_size]
     
-    if return_dv:
-        return_list.append(dv)
     if return_y:
         return_list.append(y)
     if return_frame_index:
@@ -283,7 +249,7 @@ class dv_y_wav_subwin_configuration(dv_y_configuration):
         self.batch_output_form = 'mean' # Method to convert from SBD to SD
         self.finetune_model = False
         # self.learning_rate  = 0.0001
-        # self.prev_nnets_file_name = '/home/dawna/tts/mw545/TorchDV/dv_wav_subwin/dv_y_wav_lr_0.000100_ReL80_LRe256BN_LRe256BN_LRe8DR_DV8S100B23T3200D1/Model'
+        # self.prev_nnets_file_name = '/home/dawna/tts/mw545/TorchDV/dv_wav_subwin/dv_y_wav_lr_0.000001_ReL80_ReL256BN_ReL256BN_ReL8DR_DV8S100B23T3200D1/Model'
         self.python_script_name = os.path.realpath(__file__)
 
         # Waveform-level input configuration
@@ -294,32 +260,32 @@ class dv_y_wav_subwin_configuration(dv_y_configuration):
         self.batch_seq_shift = 5*80
         self.seq_win_len   = 640         # Number of frames at 16kHz; 640 for 40ms
         self.seq_win_shift = 80
-        self.seq_num_win   = int((self.batch_seq_len - self.seq_win_len) / self.seq_win_shift) + 1
+        # self.seq_num_win   = int((self.batch_seq_len - self.seq_win_len) / self.seq_win_shift) + 1
 
         self.batch_num_spk = 100
         self.dv_dim = 8
         self.nn_layer_config_list = [
             # Must contain: type, size; num_channels, dropout_p are optional, default 0, 1
-            {'type':'ReLUSubWin', 'size':80, 'win_len':self.seq_win_len, 'num_win':self.seq_num_win, 'dropout_p':0, 'batch_norm':False},
+            # {'type':'LReLUSubWin', 'size':80, 'win_len':self.seq_win_len, 'num_win':self.seq_num_win, 'dropout_p':0, 'batch_norm':False},
+            {'type':'ReLUSubWin_ST', 'size':80, 'win_len_shift_list':[[self.batch_seq_len, self.batch_seq_shift], [self.seq_win_len, self.seq_win_shift]], 'total_length':self.batch_seq_total_len, 'dropout_p':0, 'batch_norm':False},
             {'type':'LReLUDV', 'size':256, 'dropout_p':0, 'batch_norm':True},
             {'type':'LReLUDV', 'size':256, 'dropout_p':0, 'batch_norm':True},
             {'type':'LReLUDV', 'size':self.dv_dim, 'dropout_p':0.2, 'batch_norm':False}
         ]
 
         # self.gpu_id = 'cpu'
-        self.gpu_id = 1
+        self.gpu_id = 2
 
-        from modules_torch import DV_Y_Wav_SubWin_model
-        self.dv_y_model_class = DV_Y_Wav_SubWin_model
+        from modules_torch import DV_Y_ST_model
+        self.dv_y_model_class = DV_Y_ST_model
         # from exp_mw545.exp_dv_wav_baseline import make_feed_dict_y_wav_cmp_test
         self.make_feed_dict_method_train = make_feed_dict_y_wav_subwin_train
         self.make_feed_dict_method_test  = make_feed_dict_y_wav_subwin_test
         self.make_feed_dict_method_distance  = make_feed_dict_y_wav_subwin_distance
         self.auto_complete(cfg)
 
-
-    def additional_action_epoch(self, logger, dv_y_model):
-        pass
+    def reload_model_param(self):
+        self.nn_layer_config_list[0] = {'type':'ReLUSubWin_ST', 'size':80, 'win_len_shift_list':[[self.batch_seq_len, self.batch_seq_shift], [self.seq_win_len, self.seq_win_shift]], 'total_length':self.batch_seq_total_len, 'dropout_p':0, 'batch_norm':False}
 
 def train_dv_y_wav_model(cfg, dv_y_cfg=None):
     if dv_y_cfg is None: dv_y_cfg = dv_y_wav_subwin_configuration(cfg)
