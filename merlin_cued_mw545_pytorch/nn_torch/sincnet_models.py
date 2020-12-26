58fcbeeb3b4526de458293b452f008fda9bfd3b6
moquan <moquan@users.noreply.github.com>
2020-12-27 00:16:14 +0800

    Update sincnet_models.py

--- merlin_cued_mw545_pytorch/nn_torch/sincnet_models.py
+++ merlin_cued_mw545_pytorch/nn_torch/sincnet_models.py
@@ -12,7 +12,7 @@ def flip(x, dim):
     x = x.contiguous()
     x = x.view(-1, *xsize[dim:])
     x = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1, 
-                      -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
+                    -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
     return x.view(xsize)
 
 
@@ -155,7 +155,7 @@ class SincConv_fast(nn.Module):
 
         return F.conv1d(waveforms, self.filters, stride=self.stride,
                         padding=self.padding, dilation=self.dilation,
-                         bias=None, groups=1) 
+                        bias=None, groups=1) 
 
 
         
@@ -222,26 +222,26 @@ class sinc_conv(nn.Module):
 
 def act_fun(act_type):
 
- if act_type=="relu":
-    return nn.ReLU()
-            
- if act_type=="tanh":
-    return nn.Tanh()
-            
- if act_type=="sigmoid":
-    return nn.Sigmoid()
-           
- if act_type=="leaky_relu":
-    return nn.LeakyReLU(0.2)
-            
- if act_type=="elu":
-    return nn.ELU()
-                     
- if act_type=="softmax":
-    return nn.LogSoftmax(dim=1)
-        
- if act_type=="linear":
-    return nn.LeakyReLU(1) # initializzed like this, but not used in forward!
+    if act_type=="relu":
+        return nn.ReLU()
+              
+    if act_type=="tanh":
+        return nn.Tanh()
+              
+    if act_type=="sigmoid":
+        return nn.Sigmoid()
+             
+    if act_type=="leaky_relu":
+        return nn.LeakyReLU(0.2)
+              
+    if act_type=="elu":
+        return nn.ELU()
+                       
+    if act_type=="softmax":
+        return nn.LogSoftmax(dim=1)
+          
+    if act_type=="linear":
+        return nn.LeakyReLU(1) # initializzed like this, but not used in forward!
             
             
 class LayerNorm(nn.Module):
@@ -282,11 +282,11 @@ class MLP(nn.Module):
        
         # input layer normalization
         if self.fc_use_laynorm_inp:
-           self.ln0=LayerNorm(self.input_dim)
+            self.ln0=LayerNorm(self.input_dim)
           
         # input batch normalization    
         if self.fc_use_batchnorm_inp:
-           self.bn0=nn.BatchNorm1d([self.input_dim],momentum=0.05)
+            self.bn0=nn.BatchNorm1d([self.input_dim],momentum=0.05)
            
            
         self.N_fc_lay=len(self.fc_lay)
@@ -297,169 +297,166 @@ class MLP(nn.Module):
         
         for i in range(self.N_fc_lay):
             
-         # dropout
-         self.drop.append(nn.Dropout(p=self.fc_drop[i]))
-         
-         # activation
-         self.act.append(act_fun(self.fc_act[i]))
-         
-         
-         add_bias=True
-         
-         # layer norm initialization
-         self.ln.append(LayerNorm(self.fc_lay[i]))
-         self.bn.append(nn.BatchNorm1d(self.fc_lay[i],momentum=0.05))
-         
-         if self.fc_use_laynorm[i] or self.fc_use_batchnorm[i]:
-             add_bias=False
+            # dropout
+            self.drop.append(nn.Dropout(p=self.fc_drop[i]))
+           
+            # activation
+            self.act.append(act_fun(self.fc_act[i]))
+           
+           
+            add_bias=True
+           
+            # layer norm initialization
+            self.ln.append(LayerNorm(self.fc_lay[i]))
+            self.bn.append(nn.BatchNorm1d(self.fc_lay[i],momentum=0.05))
+           
+            if self.fc_use_laynorm[i] or self.fc_use_batchnorm[i]:
+                add_bias=False
          
               
-         # Linear operations
-         self.wx.append(nn.Linear(current_input, self.fc_lay[i],bias=add_bias))
+            # Linear operations
+            self.wx.append(nn.Linear(current_input, self.fc_lay[i],bias=add_bias))
          
-         # weight initialization
-         self.wx[i].weight = torch.nn.Parameter(torch.Tensor(self.fc_lay[i],current_input).uniform_(-np.sqrt(0.01/(current_input+self.fc_lay[i])),np.sqrt(0.01/(current_input+self.fc_lay[i]))))
-         self.wx[i].bias = torch.nn.Parameter(torch.zeros(self.fc_lay[i]))
+            # weight initialization
+            self.wx[i].weight = torch.nn.Parameter(torch.Tensor(self.fc_lay[i],current_input).uniform_(-np.sqrt(0.01/(current_input+self.fc_lay[i])),np.sqrt(0.01/(current_input+self.fc_lay[i]))))
+            self.wx[i].bias = torch.nn.Parameter(torch.zeros(self.fc_lay[i]))
          
-         current_input=self.fc_lay[i]
+            current_input=self.fc_lay[i]
          
          
     def forward(self, x):
         
-      # Applying Layer/Batch Norm
-      if bool(self.fc_use_laynorm_inp):
-        x=self.ln0((x))
-        
-      if bool(self.fc_use_batchnorm_inp):
-        x=self.bn0((x))
-        
-      for i in range(self.N_fc_lay):
+        # Applying Layer/Batch Norm
+        if bool(self.fc_use_laynorm_inp):
+            x=self.ln0((x))
+          
+        if bool(self.fc_use_batchnorm_inp):
+            x=self.bn0((x))
+          
+        for i in range(self.N_fc_lay):
 
-        if self.fc_act[i]!='linear':
-            
-          if self.fc_use_laynorm[i]:
-           x = self.drop[i](self.act[i](self.ln[i](self.wx[i](x))))
-          
-          if self.fc_use_batchnorm[i]:
-           x = self.drop[i](self.act[i](self.bn[i](self.wx[i](x))))
-          
-          if self.fc_use_batchnorm[i]==False and self.fc_use_laynorm[i]==False:
-           x = self.drop[i](self.act[i](self.wx[i](x)))
-           
-        else:
-          if self.fc_use_laynorm[i]:
-           x = self.drop[i](self.ln[i](self.wx[i](x)))
-          
-          if self.fc_use_batchnorm[i]:
-           x = self.drop[i](self.bn[i](self.wx[i](x)))
-          
-          if self.fc_use_batchnorm[i]==False and self.fc_use_laynorm[i]==False:
-           x = self.drop[i](self.wx[i](x)) 
-          
-      return x
+            if self.fc_act[i]!='linear':
+                
+                if self.fc_use_laynorm[i]:
+                    x = self.drop[i](self.act[i](self.ln[i](self.wx[i](x))))
+                
+                if self.fc_use_batchnorm[i]:
+                    x = self.drop[i](self.act[i](self.bn[i](self.wx[i](x))))
+                
+                if self.fc_use_batchnorm[i]==False and self.fc_use_laynorm[i]==False:
+                    x = self.drop[i](self.act[i](self.wx[i](x)))
+               
+            else:
+                if self.fc_use_laynorm[i]:
+                    x = self.drop[i](self.ln[i](self.wx[i](x)))
+                
+                if self.fc_use_batchnorm[i]:
+                    x = self.drop[i](self.bn[i](self.wx[i](x)))
+                
+                if self.fc_use_batchnorm[i]==False and self.fc_use_laynorm[i]==False:
+                    x = self.drop[i](self.wx[i](x)) 
+              
+        return x
 
 
 
 class SincNet(nn.Module):
     
     def __init__(self,options):
-       super(SincNet,self).__init__()
+        super(SincNet,self).__init__()
     
-       self.cnn_N_filt=options['cnn_N_filt']
-       self.cnn_len_filt=options['cnn_len_filt']
-       self.cnn_max_pool_len=options['cnn_max_pool_len']
+        self.cnn_N_filt=options['cnn_N_filt']
+        self.cnn_len_filt=options['cnn_len_filt']
+        self.cnn_max_pool_len=options['cnn_max_pool_len']
        
        
-       self.cnn_act=options['cnn_act']
-       self.cnn_drop=options['cnn_drop']
+        self.cnn_act=options['cnn_act']
+        self.cnn_drop=options['cnn_drop']
        
-       self.cnn_use_laynorm=options['cnn_use_laynorm']
-       self.cnn_use_batchnorm=options['cnn_use_batchnorm']
-       self.cnn_use_laynorm_inp=options['cnn_use_laynorm_inp']
-       self.cnn_use_batchnorm_inp=options['cnn_use_batchnorm_inp']
+        self.cnn_use_laynorm=options['cnn_use_laynorm']
+        self.cnn_use_batchnorm=options['cnn_use_batchnorm']
+        self.cnn_use_laynorm_inp=options['cnn_use_laynorm_inp']
+        self.cnn_use_batchnorm_inp=options['cnn_use_batchnorm_inp']
        
-       self.input_dim=int(options['input_dim'])
+        self.input_dim=int(options['input_dim'])
        
-       self.fs=options['fs']
+        self.fs=options['fs']
        
-       self.N_cnn_lay=len(options['cnn_N_filt'])
-       self.conv  = nn.ModuleList([])
-       self.bn  = nn.ModuleList([])
-       self.ln  = nn.ModuleList([])
-       self.act = nn.ModuleList([])
-       self.drop = nn.ModuleList([])
+        self.N_cnn_lay=len(options['cnn_N_filt'])
+        self.conv  = nn.ModuleList([])
+        self.bn  = nn.ModuleList([])
+        self.ln  = nn.ModuleList([])
+        self.act = nn.ModuleList([])
+        self.drop = nn.ModuleList([])
        
              
-       if self.cnn_use_laynorm_inp:
-           self.ln0=LayerNorm(self.input_dim)
+        if self.cnn_use_laynorm_inp:
+            self.ln0=LayerNorm(self.input_dim)
            
-       if self.cnn_use_batchnorm_inp:
-           self.bn0=nn.BatchNorm1d([self.input_dim],momentum=0.05)
+        if self.cnn_use_batchnorm_inp:
+            self.bn0=nn.BatchNorm1d([self.input_dim],momentum=0.05)
            
-       current_input=self.input_dim 
+        current_input=self.input_dim 
        
-       for i in range(self.N_cnn_lay):
+        for i in range(self.N_cnn_lay):
          
-         N_filt=int(self.cnn_N_filt[i])
-         len_filt=int(self.cnn_len_filt[i])
-         
-         # dropout
-         self.drop.append(nn.Dropout(p=self.cnn_drop[i]))
-         
-         # activation
-         self.act.append(act_fun(self.cnn_act[i]))
-                    
-         # layer norm initialization         
-         self.ln.append(LayerNorm([N_filt,int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i])]))
+            N_filt=int(self.cnn_N_filt[i])
+            len_filt=int(self.cnn_len_filt[i])
+           
+            # dropout
+            self.drop.append(nn.Dropout(p=self.cnn_drop[i]))
+           
+            # activation
+            self.act.append(act_fun(self.cnn_act[i]))
+                      
+            # layer norm initialization         
+            self.ln.append(LayerNorm([N_filt,int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i])]))
 
-         self.bn.append(nn.BatchNorm1d(N_filt,int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i]),momentum=0.05))
+            self.bn.append(nn.BatchNorm1d(N_filt,int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i]),momentum=0.05))
             
 
-         if i==0:
-          self.conv.append(SincConv_fast(self.cnn_N_filt[0],self.cnn_len_filt[0],self.fs))
+            if i==0:
+                self.conv.append(SincConv_fast(self.cnn_N_filt[0],self.cnn_len_filt[0],self.fs))
               
-         else:
-          self.conv.append(nn.Conv1d(self.cnn_N_filt[i-1], self.cnn_N_filt[i], self.cnn_len_filt[i]))
+            else:
+                self.conv.append(nn.Conv1d(self.cnn_N_filt[i-1], self.cnn_N_filt[i], self.cnn_len_filt[i]))
           
-         current_input=int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i])
+            current_input=int((current_input-self.cnn_len_filt[i]+1)/self.cnn_max_pool_len[i])
 
          
-       self.out_dim=current_input*N_filt
+        self.out_dim=current_input*N_filt
 
 
 
     def forward(self, x):
-       batch=x.shape[0]
-       seq_len=x.shape[1]
-       
-       if bool(self.cnn_use_laynorm_inp):
-        x=self.ln0((x))
-        
-       if bool(self.cnn_use_batchnorm_inp):
-        x=self.bn0((x))
-        
-       x=x.view(batch,1,seq_len)
+        batch=x.shape[0]
+        seq_len=x.shape[1]
+
+        if bool(self.cnn_use_laynorm_inp):
+            x=self.ln0((x))
+
+        if bool(self.cnn_use_batchnorm_inp):
+            x=self.bn0((x))
+
+        x=x.view(batch,1,seq_len)
 
        
-       for i in range(self.N_cnn_lay):
+        for i in range(self.N_cnn_lay):
            
-         if self.cnn_use_laynorm[i]:
-          if i==0:
-           x = self.drop[i](self.act[i](self.ln[i](F.max_pool1d(torch.abs(self.conv[i](x)), self.cnn_max_pool_len[i]))))  
-          else:
-           x = self.drop[i](self.act[i](self.ln[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i]))))   
-          
-         if self.cnn_use_batchnorm[i]:
-          x = self.drop[i](self.act[i](self.bn[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i]))))
+            if self.cnn_use_laynorm[i]:
+                if i==0:
+                    x = self.drop[i](self.act[i](self.ln[i](F.max_pool1d(torch.abs(self.conv[i](x)), self.cnn_max_pool_len[i]))))  
+                else:
+                    x = self.drop[i](self.act[i](self.ln[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i]))))   
+            
+            if self.cnn_use_batchnorm[i]:
+                x = self.drop[i](self.act[i](self.bn[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i]))))
 
-         if self.cnn_use_batchnorm[i]==False and self.cnn_use_laynorm[i]==False:
-          x = self.drop[i](self.act[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i])))
+            if self.cnn_use_batchnorm[i]==False and self.cnn_use_laynorm[i]==False:
+                x = self.drop[i](self.act[i](F.max_pool1d(self.conv[i](x), self.cnn_max_pool_len[i])))
 
        
-       x = x.view(batch,-1)
+        x = x.view(batch,-1)
 
-       return x
-   
-
-    
+        return x  
    
