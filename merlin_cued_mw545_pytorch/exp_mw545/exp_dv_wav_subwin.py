26420a46aa98d594ff4a89451324ed3bebc41227
moquan <moquan@users.noreply.github.com>
2021-01-08 20:20:34 +0800

    Update exp_dv_wav_subwin.py

--- merlin_cued_mw545_pytorch/exp_mw545/exp_dv_wav_subwin.py
+++ merlin_cued_mw545_pytorch/exp_mw545/exp_dv_wav_subwin.py
@@ -15,7 +15,7 @@ class dv_y_wav_subwin_configuration(dv_y_configuration):
 
         self.use_voiced_only = False   # Use voiced regions only
         self.use_voiced_threshold = 1. # Percentage of voiced required
-        self.finetune_model = False
+        self.retrain_model = False
         self.learning_rate  = 0.00001
         # self.prev_nnets_file_name = ''
         self.python_script_name = os.path.realpath(__file__)
@@ -26,9 +26,8 @@ class dv_y_wav_subwin_configuration(dv_y_configuration):
         self.init_wav_data()
         # self.out_feat_list = ['wav_ST', 'f_SBM', 'tau_SBM', 'vuv_SBM']
         self.out_feat_list = ['wav_SBT', 'f_SBM', 'tau_SBM', 'vuv_SBM']
-# 
-        self.input_data_dim['T_M'] = 640
-        self.input_data_dim['M_shift'] = 80
+        self.input_data_dim['T_M'] = 160
+        self.input_data_dim['M_shift'] = 40
         
 
         self.dv_dim = 2048
@@ -37,8 +36,8 @@ class dv_y_wav_subwin_configuration(dv_y_configuration):
             {'type': 'Tensor_Reshape', 'io_name': 'wav_SBT_2_wav_SBMT', 'win_len_shift':[self.input_data_dim['T_M'], self.input_data_dim['M_shift']]},
             {'type': 'DW3', 'size':80, 'dropout_p':0, 'batch_norm':False},
             {'type': 'Tensor_Reshape', 'io_name': 'h_SBMD_2_h_SBD'},     # h_SBD
-            {'type': 'LReLU', 'size':2048, 'dropout_p':0, 'batch_norm':True},
-            {'type': 'LReLU', 'size':2048, 'dropout_p':0, 'batch_norm':True},
+            {'type': 'LReLU', 'size':256*8, 'dropout_p':0, 'batch_norm':True},
+            {'type': 'LReLU', 'size':256*8, 'dropout_p':0, 'batch_norm':True},
             {'type': 'Linear', 'size':self.dv_dim, 'dropout_p':0, 'batch_norm':True}
         ]
 
@@ -58,4 +57,7 @@ def test_model(cfg, dv_y_cfg=None):
 
     from exp_mw545.exp_dv_y import Build_DV_Y_Testing
     dv_y_model_test = Build_DV_Y_Testing(cfg, dv_y_cfg)
-    dv_y_model_test.vuv_loss_test()
+    fig_file_name = '/home/dawna/tts/mw545/Export_Temp/PNG_out/vuv_loss_subwin.png'
+    dv_y_model_test.vuv_loss_test(fig_file_name)
+    fig_file_name = '/home/dawna/tts/mw545/Export_Temp/PNG_out/pos_subwin.png'
+    dv_y_model_test.positional_test(fig_file_name)
