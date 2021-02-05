7e94449b49c2757d8c91a51b11a2410d69ebe928
moquan <moquan@users.noreply.github.com>
2021-02-05 22:22:21 +0800

    Update setup.sh

--- merlin_cued_mw545_pytorch/debug_nausicaa/setup.sh
+++ merlin_cued_mw545_pytorch/debug_nausicaa/setup.sh
@@ -7,3 +7,5 @@ mkdir log
 ln -s /data/vectra2/tts/mw545/Data/data_voicebank data
 ln -s /home/dawna/tts/mw545/TorchDV/tools/merlin_cued_mw545/exp_mw545
 ln -s /home/dawna/tts/mw545/TorchDV/tools/merlin_cued_mw545/run_nn_iv_batch_T4_DV.py
+
+rm run_nn_iv_batch_T4_DV.py; cp /home/dawna/tts/mw545/TorchDV/tools/merlin_cued_mw545/run_nn_iv_batch_T4_DV.py .
